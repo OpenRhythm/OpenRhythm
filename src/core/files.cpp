72a47798b80ea6caedb0036cf13188ea736dc80f
mecwerks <mecwerks@gmail.com>
2015-03-16 21:35:04 -0400

    Simplified vfs_read_file.

--- src/core/files.cpp
+++ src/core/files.cpp
@@ -5,19 +5,19 @@
 namespace MgCore
 {
 	std::string read_file(std::string filename)
-        {
-                std::ifstream in(filename, std::ios::in | std::ios_base::ate);
-                if (in) {
-                        std::string contents;
-                        contents.resize(in.tellg());
-                        in.seekg(0, std::ios::beg);
-                        in.read(&contents[0], contents.size());
-                        in.close();
-                        return contents;
-                } else {
-                        return "";
-                }
-        }
+    {
+        std::ifstream in(filename, std::ios::in | std::ios_base::ate);
+        if (in) {
+            std::string contents;
+            contents.resize(in.tellg());
+            in.seekg(0, std::ios::beg);
+            in.read(&contents[0], contents.size());
+            in.close();
+            return contents;
+        } else {
+        	return "";
+		}
+	}
 
 	std::string vfs_read_file(std::string filename)
 	{
@@ -25,10 +25,9 @@ namespace MgCore
 
 		if ( vf && vf->open("r") ) {
 			std::string contents;
-			char buff[vf->size()];
-			size_t len = vf->read(buff, sizeof(buff));
+			contents.resize( vf->size() );
+			vf->read(&contents[0], contents.size());
 			vf->close();
-			contents = buff;
 			return contents;
 		} else {
 			return "";
