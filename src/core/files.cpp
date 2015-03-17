0aa40a797e997b8b949edc730a54d067705ddd3d
mecwerks <mecwerks@gmail.com>
2015-03-16 21:45:44 -0400

    One more time

--- src/core/files.cpp
+++ src/core/files.cpp
@@ -4,33 +4,33 @@
 
 namespace MgCore
 {
-	std::string read_file(std::string filename)
-	{
-		std::ifstream in(filename, std::ios::in | std::ios_base::ate);
-		if (in) {
-			std::string contents;
-			contents.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&contents[0], contents.size());
-			in.close();
-			return contents;
-		} else {
-			return "";
-		}
-	}
+    std::string read_file(std::string filename)
+    {
+        std::ifstream in(filename, std::ios::in | std::ios_base::ate);
+        if (in) {
+            std::string contents;
+            contents.resize(in.tellg());
+            in.seekg(0, std::ios::beg);
+            in.read(&contents[0], contents.size());
+            in.close();
+            return contents;
+        } else {
+            return "";
+        }
+    }
 
-	std::string vfs_read_file(std::string filename)
-	{
-		ttvfs::File *vf = VFS.GetFile( filename.c_str() );
+    std::string vfs_read_file(std::string filename)
+    {
+        ttvfs::File *vf = VFS.GetFile( filename.c_str() );
 
-		if ( vf && vf->open("r") ) {
-			std::string contents;
-			contents.resize( vf->size() );
-			vf->read(&contents[0], contents.size());
-			vf->close();
-			return contents;
-		} else {
-			return "";
-		}
-	}
+        if ( vf && vf->open("r") ) {
+            std::string contents;
+            contents.resize( vf->size() );
+            vf->read(&contents[0], contents.size());
+            vf->close();
+            return contents;
+        } else {
+            return "";
+        }
+    }
 }
