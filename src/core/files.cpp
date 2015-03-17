6c096864882791dacc790e31d5b1c8f7646a56e1
mecwerks <mecwerks@gmail.com>
2015-03-16 21:40:12 -0400

    Stupid indents

--- src/core/files.cpp
+++ src/core/files.cpp
@@ -5,17 +5,17 @@
 namespace MgCore
 {
 	std::string read_file(std::string filename)
-    {
-        std::ifstream in(filename, std::ios::in | std::ios_base::ate);
-        if (in) {
-            std::string contents;
-            contents.resize(in.tellg());
-            in.seekg(0, std::ios::beg);
-            in.read(&contents[0], contents.size());
-            in.close();
-            return contents;
-        } else {
-        	return "";
+	{
+		std::ifstream in(filename, std::ios::in | std::ios_base::ate);
+		if (in) {
+			std::string contents;
+			contents.resize(in.tellg());
+			in.seekg(0, std::ios::beg);
+			in.read(&contents[0], contents.size());
+			in.close();
+			return contents;
+		} else {
+			return "";
 		}
 	}
 
