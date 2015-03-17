4bc83c80dcf8d2a9f74e0222520c3dcaf48f72c4
Matthew Sitton <matthewsitton@gmail.com>
2015-03-17 03:39:01 -0500

    Some changes to the mac/linux build script.

--- release-build.sh
+++ release-build.sh
@@ -6,6 +6,5 @@ if [ "$1" = "--noapp" ]
 then
 	export NO_APP="True"
 fi
-
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release ..
 cmake --build .
