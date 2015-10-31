3c9aa427b4771c7648cd58ef6789b9430ed7b78e
Matthew Sitton <matthewsitton@gmail.com>
2015-10-31 05:06:00 -0500

    Add string utilities.

---
+++ src/core/stringutils.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include "stringutils.hpp"
+
+int stringCount(const std::string& str, const std::string& substr)
+{
+    std::size_t start = 0;
+    int count = 0;
+    while (true){
+        start = str.find(substr, start);
+        if (start != std::string::npos) {
+            count++;
+            start += substr.size();
+        } else {
+            break;
+        }
+    }
+
+    return count;
+}
+
+std::string stringJoin(const std::vector<std::string>& strElements, const std::string& delimiter)
+{
+    int strSize = 0;
+    for (const auto &str : strElements)
+    {
+        strSize += str.size();
+    }
+    std::string output;
+    output.reserve(strSize);
+    const std::string &vec_back = strElements.back();
+    std::size_t vecSize = strElements.size();
+    for (const auto &str : strElements)
+    {
+        output.append(str);
+        if (str != vec_back)
+        {
+            output.append(delimiter);
+        }
+    }
+    return output;
+}
+
+std::vector<std::string> stringSplit(const std::string& str, const std::string& delimiter)
+{
+    std::vector<std::string> split;
+
+    std::size_t start = 0;
+    std::size_t end = 0;
+    bool running = true;
+
+    while (running) {
+        end = str.find(delimiter, start);
+        if (end == std::string::npos) {
+            end = str.size();
+            running = false;
+        }
+        split.push_back(str.substr(start, end-start));
+        start = end + delimiter.size();
+    }
+
+    return split;
+}
---
+++ src/core/stringutils.hpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+
+int stringCount(const std::string& str, const std::string& substr);
+
+std::string stringJoin(const std::vector<std::string>& strElements, const std::string& delimiter);
+
+std::vector<std::string> stringSplit(const std::string& str, const std::string& delimiter);
