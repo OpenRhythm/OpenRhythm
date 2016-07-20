a31bc6700f7d147918623f6ee57462254031a050
Félix Piédallu <felix@piedallu.me>
2016-07-20 23:13:31 +0200

    PortAudio now works with vorbisfile

---
+++ src/core/audio/output/portaudio.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <portaudio.h>
+#include "portaudio.hpp"
+
+#define NUM_SECONDS   (500)
+#define SAMPLE_RATE   (44100)
+#define FRAMES_PER_BUFFER  (64)
+
+Codec* mysong;
+/* This routine will be called by the PortAudio engine when audio is needed.
+** It may called at interrupt level on some machines so don't do anything
+** that could mess up the system like calling malloc() or free().
+*/
+static int patestCallback(  const void *inputBuffer, void *outputBuffer,
+                            unsigned long framesPerBuffer,
+                            const PaStreamCallbackTimeInfo* timeInfo,
+                            PaStreamCallbackFlags statusFlags,
+                            void *userData ) {
+    // Prevent unused variable warnings.
+    (void) timeInfo;
+    (void) statusFlags;
+    (void) inputBuffer;
+
+    mysong->readBuffer((char*)outputBuffer, 4*framesPerBuffer);
+
+    return paContinue;
+}
+
+// This routine is called by portaudio when playback is done.
+static void StreamFinished(void* userData) {
+    printf( "Stream Completed.");
+}
+
+
+int pa_main(Codec* thesong) {
+    mysong = thesong;
+
+    PaStreamParameters outputParameters;
+    PaStream *stream;
+    PaError err;
+
+    printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
+
+    err = Pa_Initialize();
+    if( err != paNoError ) goto error;
+
+    outputParameters.device = Pa_GetDefaultOutputDevice(); // default output device
+    if (outputParameters.device == paNoDevice) {
+        fprintf(stderr,"Error: No default output device.\n");
+        goto error;
+    }
+    outputParameters.channelCount = 2;       // stereo output
+    outputParameters.sampleFormat = paInt16; // 32 bit floating point output
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+
+    err = Pa_OpenStream(
+            &stream,
+            NULL, // no input
+            &outputParameters,
+            SAMPLE_RATE,
+            FRAMES_PER_BUFFER,
+            paClipOff,      // we won't output out of range samples so don't bother clipping them
+            patestCallback,
+            NULL);
+    if( err != paNoError ) goto error;
+
+    err = Pa_SetStreamFinishedCallback( stream, &StreamFinished );
+    if( err != paNoError ) goto error;
+
+    err = Pa_StartStream( stream );
+    if( err != paNoError ) goto error;
+
+    printf("Play for %d seconds.\n", NUM_SECONDS );
+    Pa_Sleep( NUM_SECONDS * 1000 );
+
+    err = Pa_StopStream( stream );
+    if( err != paNoError ) goto error;
+
+    err = Pa_CloseStream( stream );
+    if( err != paNoError ) goto error;
+
+    Pa_Terminate();
+    printf("Test finished.\n");
+
+    return err;
+error:
+    Pa_Terminate();
+    fprintf( stderr, "An error occured while using the portaudio stream\n" );
+    fprintf( stderr, "Error number: %d\n", err );
+    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
+    return err;
+}
---
+++ src/core/audio/output/portaudio.hpp
@@ -0,0 +1,8 @@
+#ifndef PORTAUDIO_HPP
+#define PORTAUDIO_HPP
+
+#include "codec.hpp"
+
+int pa_main(Codec *thesong);
+
+#endif
