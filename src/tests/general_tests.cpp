ef787fadf2ec298098111dad79dde94d86363c42
Félix Piédallu <felix@piedallu.me>
2016-10-29 16:49:01 +0200

    added test for configuration

--- src/tests/general_tests.cpp
+++ src/tests/general_tests.cpp
@@ -1,10 +1,20 @@
-#include "os_utils.hpp"
-
+#include <iostream>
+#include <yaml-cpp/yaml.h>
 #include <libintl.h>
 #define _(STRING) gettext(STRING)
 
+#include "configuration/parameter.hpp"
+#include "configuration.hpp"
+
 
 int main(int argc, char const *argv[]) {
-    std::cout << get_config_directory() << std::endl;
+    //std::cout << get_config_directory() << std::endl;
+    readConfiguration();
+    std::cout << debug_song1.getValue() << std::endl;;
+
+
+
+
+    writeConfigurationFile("");
     return 0;
 }
