a5cfaf10edacf14d4c8fa0771b5669a0931a3e55
Matthew Sitton <matthewsitton@gmail.com>
2016-12-03 04:34:39 -0600

    Fix Findfmt.cmake

--- cmake/Findfmt.cmake
+++ cmake/Findfmt.cmake
@@ -10,12 +10,11 @@ find_path(FMT_INCLUDE_DIR "fmt/format.h")
 find_path(FMT_INCLUDE_DIR "fmt/format.h"
   HINTS $ENV{FMT_ROOT}/include)
 
-
 find_library(FMT_LIBRARY NAMES fmt)
 find_library(FMT_LIBRARY NAMES fmt
     HINTS $ENV{FMT_ROOT}/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FMT DEFAULT_MSG FMT_LIBRARY FMT_INCLUDE_DIR)
+find_package_handle_standard_args(fmt DEFAULT_MSG FMT_LIBRARY FMT_INCLUDE_DIR)
 
-mark_as_advanced(FMT_INCLUDE_DIR)
+mark_as_advanced(FMT_LIBRARY FMT_INCLUDE_DIR)
