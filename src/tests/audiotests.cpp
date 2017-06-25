672a3b623969206597503c635958f6614c9e6662
Matthew Sitton <matthewsitton@gmail.com>
2017-06-25 15:26:49 -0500

    add audio test(it works).

--- src/tests/audiotests.cpp
+++ src/tests/audiotests.cpp
@@ -1,27 +1,16 @@
 #include "config.hpp"
 
 #include <iostream>
-#include <chrono>
 #include <thread>
-#include "core/audio/codecs/vorbis.hpp"
-#include "core/audio/streams/resample.hpp"
-#include "core/audio/output/soundio.hpp"
 
 #include <spdlog/spdlog.h>
 
+#include "core/audio/streams.hpp"
+#include "core/audio/cubeboutput.hpp"
 
-int main(int argc, char *argv[]) {
 
-// TODO - Move this to into a function/class within a future i18n module.
-#if defined(TRANSLATION_ENABLED)
-    // Setting the i18n environment
-    setlocale (LC_ALL, "");
-    bindtextdomain ("openrhythm", LOCALE_DIR);
-    textdomain ("openrhythm");
-#endif
 
-    // Example of i18n usage
-    printf(_("Hello World\n"));
+int main() {
 
     std::shared_ptr<spdlog::logger> logger;
 
@@ -41,71 +30,16 @@ int main(int argc, char *argv[]) {
         logger->set_level(spdlog::level::debug);
 
     } catch (const spdlog::spdlog_ex& err) {
-        std::cout << _("Logging Failed: ") << err.what() << std::endl;
+        std::cout << "Logging Failed: " << err.what() << std::endl;
         return 1;
     }
+    ORCore::SineStream prod;
+    ORCore::CubebOutput out;
 
-    std::string OggTestFile     = "song.ogg";
-    std::string OggAnotherFile  = "guitar.ogg";
+    out.set_source(&prod);
+    out.start();
 
-    // You may change that to change your config
-    int outputSampleRate = 44100;
-
-    // Initialize the audio output
-    auto soundOutput = new ORCore::SoundIoOutput();
-    soundOutput->connect_default_output_device();
-    soundOutput->open_stream_with_sample_rate(outputSampleRate);
-
-
-    // First ogg file
-    try {
-        ORCore::VorbisInput *mysong = new ORCore::VorbisInput(OggTestFile);
-        mysong->open();
-
-        int songSampleRate = mysong->getSampleRate();
-
-        auto *resamplerstream =
-            new ORCore::ResamplerStream(mysong, SRC_SINC_MEDIUM_QUALITY);
-        resamplerstream->setInputSampleRate(songSampleRate);
-        resamplerstream->setOutputSampleRate(outputSampleRate);
-
-        logger->info(_("SongSampleRate: {}"), songSampleRate);
-
-        logger->debug(_("addstream resamplerstream"));
-        soundOutput->add_stream(resamplerstream);
-    } catch (const std::runtime_error& err) {
-        std::cout << _("opening ogg vorbis file failed: ") << err.what() << std::endl;
-    }
-
-
-
-    // Add another sound to test multiple streams output
-    try {
-        logger->debug("add audio file \"{}\"", OggAnotherFile);
-
-
-        ORCore::VorbisInput *anotherOgg = new ORCore::VorbisInput(OggAnotherFile);
-        anotherOgg->open();
-
-        int anotherOggSampleRate = anotherOgg->getSampleRate();
-
-        auto *anotherResamplerstream =
-            new ORCore::ResamplerStream(anotherOgg, SRC_SINC_MEDIUM_QUALITY);
-        anotherResamplerstream->setInputSampleRate(anotherOggSampleRate);
-        anotherResamplerstream->setOutputSampleRate(outputSampleRate);
-
-        soundOutput->add_stream(anotherResamplerstream);
-    } catch (const std::runtime_error& err) {
-        std::cout << _("opening ogg vorbis file failed: ") << err.what() << std::endl;
-    }
-
-
-
-    // Wait a while then stop all that
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-    soundOutput->destroy();
-    soundOutput->disconnect_device();
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
+    // Wait 10 seconds then exit..
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    out.stop();
 }
